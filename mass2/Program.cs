bf68b8562895faad2b8197a556aafd602fee9724
stacy2906 <anastasiya.tkachyk.290606@gmail.com>
2023-02-07 21:05:24 +0200

    Create Program.cs
    add mass2

---
+++ mass2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mass2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { //Console.Write("Размер массива: ");
+          //byte size = Convert.ToByte(Console.ReadLine());
+          //int[] mass = new int[size];
+          //Random rand=new Random();
+          //int i;
+          //for (i = 0; i < mass.Length; i++)
+          //{
+          //    mass[i] = rand.Next(-10, 10);
+          //    Console.WriteLine($"mass[{i}]={mass[i]}");
+          //}
+          //int k = 0;
+          ////for (i = 0; i < mass.Length; i++)
+          ////{
+          ////    if (mass[i] > 0) k++;
+          ////}
+          //foreach (int elem in mass)
+          //{
+          //    if (elem > 0) k++;
+          //}
+          //Console.WriteLine($"k={k}");
+          //Console.ReadLine();
+
+            //Console.Write("Размер массива: ");
+            //byte size = Convert.ToByte(Console.ReadLine());
+            //int[] mass = new int[size];
+            //Random rand = new Random();
+            //int i;
+            //for (i = 0; i < mass.Length; i++)
+            //{
+            //    mass[i] = rand.Next(-10, 10);
+            //}
+            //for (i = 0; i < mass.Length; i++)
+            //{
+            //    mass[i] *= 2;
+            //    Console.WriteLine($"mass[{i}]={mass[i]}");
+            //}
+            Console.ReadLine();
+        }
+    }
+}
